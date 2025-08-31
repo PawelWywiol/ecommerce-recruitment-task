1a27feaa9818efda655dda856c1e469e5da5280e
Paweł Wywioł <pawel.wywiol@gmail.com>
2025-08-31 06:12:09 +0200

    fix: update product API URLs to use slug in path

--- src/services/products/products.config.ts
+++ src/services/products/products.config.ts
@@ -1,8 +1,8 @@
 export const PRODUCTS_API_URL = 'https://api.escuelajs.co/api/v1/products';
 export const PRODUCT_BY_SLUG_API_URL = (slug: string): string =>
-  `https://api.escuelajs.co/api/v1/products/${slug}`;
+  `https://api.escuelajs.co/api/v1/products/slug/${slug}`;
 export const PRODUCT_BY_SLUG_RELATED_API_URL = (slug: string): string =>
-  `https://api.escuelajs.co/api/v1/products/${slug}/related`;
+  `https://api.escuelajs.co/api/v1/products/slug/${slug}/related`;
 
 export const PRODUCT_TITLE_MAX_LENGTH = 100;
 export const PRODUCT_TITLE_MIN_LENGTH = 1;
